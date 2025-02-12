ded5db9917dea03718083af3140aa3510aabc354
anadiazace2002 <150518852+anadiazace2002@users.noreply.github.com>
2025-02-12 18:27:25 +0100

    Update train.py

--- train.py
+++ train.py
@@ -30,7 +30,7 @@ def get_config():
     parser.add_argument('--save_dir', type=str, default='./saved_models')
     parser.add_argument('-sn', '--save_name', type=str, default='semisup')
     parser.add_argument('--resume', action='store_true')
-    parser.add_argument('--load_path', type=str)
+    parser.add_argument('--load_path', type=str, default = '/home/anadiaz/General-Framework-Weak-Supervision-GC/saved_models/noisy_label/classic_cv/imp_noisy_label_cifar100_sym_0.5_42/latest_model.pth')
     parser.add_argument('-o', '--overwrite', action='store_true', default=True)
     parser.add_argument('--use_tensorboard', action='store_true', help='Use tensorboard to plot and save curves')
     parser.add_argument('--use_wandb', action='store_true', help='Use wandb to plot and save curves')
