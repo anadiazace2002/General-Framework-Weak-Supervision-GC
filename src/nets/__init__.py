2bc0933af04ebc57cdd16b375d63b2dbcbe48e28
anadiazace2002 <150518852+anadiazace2002@users.noreply.github.com>
2025-05-21 16:55:48 +0200

    Update __init__.py

--- src/nets/__init__.py
+++ src/nets/__init__.py
@@ -1,6 +1,6 @@
 from .wrn import wrn_28_2, wrn_28_8, wrn_34_10
 from .wrn_var import wrn_var_37_2
-from .resnet import resnet34, resnet50, resnet18, resnet50_pretrained, resnet18_pretrained
+from .resnet import resnet34, resnet50, resnet18 #, resnet50_pretrained, resnet18_pretrained
 from .preact_resnet import preact_resnet18
 from .inception_resnet import inception_resnet_v2
 from .lenet import attn_lenet5, gated_attn_lenet5, lenet5, lenet5_c3
